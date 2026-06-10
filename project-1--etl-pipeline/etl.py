af41dd2f395535c1eaf1b042c60b733393e3c078
Maham-J <mahamjamil596@gmail.com>
2026-06-10 17:06:32 +0500

    Project 1: ETL pipeline with Titanic dataset

---
+++ project-1--etl-pipeline/etl.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+print("Extracting data...")
+df = pd.read_csv("titanic.csv")
+print(f"Rows loaded: {len(df)}")
+print(df.head())
+
+print("\nTransforming data...")
+df = df.drop_duplicates()
+df['age'] = df['age'].fillna(df['age'].median())
+df['embarked'] = df['embarked'].fillna(df['embarked'].mode()[0])
+df['survived'] = df['survived'].astype(bool)
+print(f"Rows after cleaning: {len(df)}")
+#LOAD
+print("\n Loading the parquet..")
+table = pa.Table.from_pandas(df)
+pq.write_table(table, "titanic clean parquet")
+print("saved to titanic_clean.parquet")
