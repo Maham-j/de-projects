7e9edf10405dc726e4175a71b6db26bb61b43d12
Maham.J <139694482+Maham-j@users.noreply.github.com>
2026-06-26 23:04:09 +0500

    Add files via upload

---
+++ project-3-dbt-bigquery/titanic_dbt/models/staging/stg_titanic.sql
@@ -0,0 +1,9 @@
+-- This model selects and cleans columns from the raw titanic table
+SELECT
+    survived,
+    pclass AS passenger_class,
+    sex,
+    age,
+    fare,
+    embarked AS port_of_embarkation
+FROM `de-projects-499016.titanic_dataset.titanic_clean`
